80c108e8a4ba5c7a13aa45c1912ec42cf68ad991
Jonathan Jansson <h23jjans@du.se>
2025-11-26 09:28:28 +0100

    Lade till random(slump), fixade några buggar

--- Labb1.py
+++ Labb1.py
@@ -2,6 +2,7 @@ import spade
 from spade.agent import Agent
 from spade.behaviour import FSMBehaviour, State
 from spade.message import Message
+import random
 
 STATE_START = "StartState"
 STATE_TEE = "TeeState"
@@ -41,10 +42,18 @@ class TeeState(State):
         print("Dags för ditt första slag! \n Välj klubba:\n " \
         "1. Driver \n 2. Järn-7 \n 3. Putter")
         choice = input("Ange klubba: 1/2/3\n>").strip()
+        slump = random.random()
         if choice == "1":
-            print("Bra val! Du tog dig till Fairway med bara 50 meter kvar till green!")
             self.agent.counter += 1
-            self.set_next_state(STATE_FAIRWAY_2)
+            if slump < 0.8:
+                print("Bra val! Du tog dig till Fairway med bara 50 meter kvar till green!")
+                self.set_next_state(STATE_FAIRWAY_2)
+            elif 0.8 <= slump < 0.99:
+                print("Ett bra val av klubba, men du slog snett och hamnade i ett svårspelat läge i roughen..")
+                self.set_next_state(STATE_ROUGH)
+            else:
+                print("Wow! Du får en drömträff och landar på green nära flagg!")
+                self.set_next_state(STATE_GREEN)
         elif choice == "2":
             print("Ok val. Du landar på fairway men har fortfarande en bit kvar till green.")
             self.agent.counter += 1
@@ -62,14 +71,26 @@ class Fairway1State(State):
         print("Du har 200 meter kvar till green, vilken klubba väljer du? \n" \
         "1. Järn 5 \n 2. Järn 7 \n 3. Putter")
         choice = input("Ange klubba: 1/2/3\n>").strip()
+        slump = random.random()
         if choice == "1":
-            print("Bra val! Du tog dig nära green, men tyvärr landade du i en bunker..")
             self.agent.counter += 1
-            self.set_next_state(STATE_BUNKER)
+            if slump < 0.85:
+                print("Bra val! Du får en bra träff, men landar i bunkern precis innan green..")
+                self.set_next_state(STATE_BUNKER)
+            elif 0.85 <= slump < 0.99:
+                print("Snyggt! Du får en bra träff och landar på green nära flagg!")
+                self.set_next_state(STATE_GREEN)
+            else:
+                print("Wow! Du får en otrolig träff, bollen landar på green och... rullar i hålet!")
+                self.set_next_state(STATE_END)
         elif choice == "2":
-            print("Du är kvar på fairway, men bara 50 meter kvar till green!")
             self.agent.counter += 1
-            self.set_next_state(STATE_FAIRWAY_2)
+            if slump < 0.9:
+                print("Du är kvar på fairway, men bara 50 meter kvar till green!")
+                self.set_next_state(STATE_FAIRWAY_2)
+            else:
+                print("Bra val, men du träffar snett och hamnar i ett svårt läge i roughen..")
+                self.set_next_state(STATE_ROUGH)
         elif choice == "3":
             print("Puttern var ett dåligt val, du är kvar på samma position.")
             self.agent.counter += 1
@@ -83,6 +104,7 @@ class Fairway2State(State):
         print("Du har bara 50 meter kvar till green! Vilken klubba väljer du? \n" \
         "1. Järn 5 \n 2. Putter \n 3. Pitch \n 4. Sand wedge")
         choice = input("Ange klubba: 1/2/3/4\n>").strip()
+        slump = random.random()
         if choice == "1":
             print("Ajdå! Du slog alldeles för långt och hamnade i roughen..")
             self.agent.counter += 1
@@ -92,27 +114,47 @@ class Fairway2State(State):
             self.agent.counter += 1
             self.set_next_state(STATE_FAIRWAY_2)
         elif choice == "3":
-            print("Bra val! Du landar på green nära flagg!")
             self.agent.counter += 1
-            self.set_next_state(STATE_GREEN)
+            if slump < 0.8:
+                print("Bra val! Du landar på green nära flagg!")
+                self.set_next_state(STATE_GREEN)
+            elif 0.8 <= slump < 0.97:
+                print("Bra val, men tyvärr duffade du och hamnade i bunkern precis innan green.")
+                self.set_next_state(STATE_BUNKER)
+            else:
+                print("Wow! Du får en otrolig träff, bollen landar på green och... rullar i hålet!")
+                self.set_next_state(STATE_END)
         elif choice == "4":
-            print("Du tar dig framåt, men landar tyvärr i bunkern precis innan green.")
             self.agent.counter += 1
-            self.set_next_state(STATE_BUNKER)
+            if slump < 0.75:
+                print("Du tar dig framåt, men landar tyvärr i bunkern precis innan green.")
+                self.set_next_state(STATE_BUNKER)
+            else:
+                print("Bra val! Bollen studsar på bunkerkanten och landar på green nära flagg!")
+                self.set_next_state(STATE_GREEN)
         else:
             print("Felaktig inmatning, försök igen.")
             self.set_next_state(STATE_FAIRWAY_2)
 
 class RoughState(State):
     async def run(self):
-        print("Du har landat i roughen och har ingen möjlighet att spela dig upp på green.\n" \
+        print("Du har landat i roughen och det ser svårt ut att ta sig upp på green.\n" \
         "Du vill spela fram dig och landa på fairway nära green. Du har 120m kvar till flagg.\n" \
         "Vilken klubba väljer du?\n" \
         "1. Järn 7 \n 2. Pitch \n 3. Putter")
         choice = input("Ange klubba: 1/2/3\n>").strip()
+        slump = random.random()
         if choice == "1":
-            print("Bra val! Men du slog lite långt och hamnade i bunkern.")
             self.agent.counter += 1
+            if slump < 0.35:
+                print("Bra val! Du slog fram dig bra och landade på fairway med bara 50 meter kvar till green.")
+                self.set_next_state(STATE_FAIRWAY_2)
+            elif 0.35 <= slump < 0.9:
+                print("Bra val, men du hamnade i bunkern precis innan green.")
+                self.set_next_state(STATE_BUNKER)
+            else:
+                print("Snyggt! Du lyckades slå bollen över bunkern och landade på green nära flagg!")
+                self.set_next_state(STATE_GREEN)
             self.set_next_state(STATE_BUNKER)
         elif choice == "2":
             print("Snyggt! Du slog fram dig bra och landade på fairway med bara 50 meter kvar till green.")
@@ -132,14 +174,19 @@ class BunkerState(State):
         "Bunkern är svårspelad, vilken klubba väljer du?\n" \
         "1. Järn 7 \n 2. Sandwedge \n 3. Hybrid 3 \n 4. putter")
         choice = input("Ange klubba: 1/2/3/4\n>").strip()
+        slump = random.random()
         if choice == "1":
             print("Du slår i kanten och bollen rullar tillbaks ner i bunkern..")
             self.agent.counter += 1
             self.set_next_state(STATE_BUNKER)
         elif choice == "2":
-            print("Bra val! Du gör ett snyggt slag upp på green och bollen stannar nära flagg!")
             self.agent.counter += 1
-            self.set_next_state(STATE_GREEN)
+            if slump < 0.9:
+                print("Bra val! Du gör ett snyggt slag upp på green och bollen stannar nära flagg!")
+                self.set_next_state(STATE_GREEN)
+            else:
+                print("Bra val! Du får perfekt träff, bollen landar på green och... rullar i hålet!")
+                self.set_next_state(STATE_END)
         elif choice == "3":
             print("Du slår i kanten och bollen rullar tillbaks ner i bunkern..")
             self.agent.counter += 1
@@ -169,20 +216,26 @@ class GreenState(State):
             self.set_next_state(STATE_GREEN)
         elif choice == "3":
             self.agent.counter += 1
-            print(f"Grattis! Du satte den och är klar med denna runda. \n" \
-            f"Du gick hålet på {self.agent.counter} slag.")
-            if self.agent.counter == 3:
-                print("Birdie!!")
-            elif self.agent.counter == 4:
-                print("Par!")
-            elif self.agent.counter == 5:
-                print("Boogey..")
-            elif self.agent.counter > 5:
-                print("Ställ dig på ranchen nästa gång..")
+            print("Bra val! Du siktar noggrant och... bollen rullar i hålet!")
             self.set_next_state(STATE_END)
+        else:
+            print("Felaktig inmatning, försök igen.")
+            self.set_next_state(STATE_GREEN)
 
 class EndState(State):
     async def run(self):
+        print(f"Grattis! Du satte den och är klar med denna runda. \n" \
+            f"Du gick hålet på {self.agent.counter} slag.")
+        if self.agent.counter == 2:
+                print("Eagle!!")
+        elif self.agent.counter == 3:
+                print("Birdie!!")
+        elif self.agent.counter == 4:
+                print("Par!")
+        elif self.agent.counter == 5:
+                print("Boogey..")
+        elif self.agent.counter > 5:
+                print("Ställ dig på ranchen nästa gång..")
         choice = input("Vill du spela igen? 'ja/nej'\n>").strip().lower()
         if choice == "ja":
             self.agent.counter = 0
@@ -210,18 +263,26 @@ class Lab1_FSM_golf_Agent(Agent):
         fsm.add_transition(source=STATE_TEE, dest=STATE_FAIRWAY_1)
         fsm.add_transition(source=STATE_TEE, dest=STATE_FAIRWAY_2)
         fsm.add_transition(source=STATE_TEE, dest=STATE_TEE)
+        fsm.add_transition(source=STATE_TEE, dest=STATE_ROUGH)
+        fsm.add_transition(source=STATE_TEE, dest=STATE_GREEN)
         fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_BUNKER)
         fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_FAIRWAY_2)
         fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_FAIRWAY_1)
+        fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_ROUGH)
+        fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_GREEN)
+        fsm.add_transition(source=STATE_FAIRWAY_1, dest=STATE_END)
         fsm.add_transition(source=STATE_FAIRWAY_2, dest=STATE_ROUGH)
         fsm.add_transition(source=STATE_FAIRWAY_2, dest=STATE_GREEN)
         fsm.add_transition(source=STATE_FAIRWAY_2, dest=STATE_FAIRWAY_2)
         fsm.add_transition(source=STATE_FAIRWAY_2, dest=STATE_BUNKER)
+        fsm.add_transition(source=STATE_FAIRWAY_2, dest=STATE_END)
         fsm.add_transition(source=STATE_ROUGH, dest=STATE_BUNKER)
         fsm.add_transition(source=STATE_ROUGH, dest=STATE_FAIRWAY_2)
         fsm.add_transition(source=STATE_ROUGH, dest=STATE_ROUGH)
+        fsm.add_transition(source=STATE_ROUGH, dest=STATE_GREEN)
         fsm.add_transition(source=STATE_BUNKER, dest=STATE_BUNKER)
         fsm.add_transition(source=STATE_BUNKER, dest=STATE_GREEN)
+        fsm.add_transition(source=STATE_BUNKER, dest=STATE_END)
         fsm.add_transition(source=STATE_GREEN, dest=STATE_BUNKER)
         fsm.add_transition(source=STATE_GREEN, dest=STATE_GREEN)
         fsm.add_transition(source=STATE_GREEN, dest=STATE_END)
